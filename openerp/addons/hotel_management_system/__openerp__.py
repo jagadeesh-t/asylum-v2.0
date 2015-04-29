524db9b99d53196a4c0e0b12353e7501d5d5d6db
thresholdsoftware <deepu.rao90@gmail.com>
2015-04-29 22:59:54 +0530

    Creation Of Configuration Files

---
+++ openerp/addons/hotel_management_system/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#
+##############################################################################
+
+{
+    'name': 'Hotel Management System',
+    'version': '1.0',
+    'author': 'Pradeep',
+    'category': 'Hotel',
+    'website': 'http://www.thresholdsoft.com/',
+    'summary': 'Hotel Management',
+    'description' : """
+                    Manage Hotel
+                    """,
+    'depends': ['base'],
+    'data': [
+             'hotel_menu.xml',
+             # 'hotel_room_type_view.xml',
+             # 'hotel_guest_type_view.xml',
+             # 'hotel_book_order_view.xml',
+             # # 'board_hotel_view.xml',
+             # 'hotel_bed_type_view.xml',
+             # 'hotel_activity_type_view.xml',
+             # # 'hotel_activity_calender_view.xml',
+             # 'hotel_room_view.xml',
+             # # 'wizard/change_bed_qty_view.xml',
+             # 'hotel_products_category_view.xml',
+             # 'hotel_product_view.xml',
+             ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
