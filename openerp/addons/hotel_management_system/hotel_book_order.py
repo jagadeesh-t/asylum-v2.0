ae2631b9f0eca9d89666e623709172b0de5bda34
thresholdsoftware <deepu.rao90@gmail.com>
2015-05-09 21:48:17 +0530

    Hotel Book Order

--- openerp/addons/hotel_management_system/hotel_book_order.py
+++ openerp/addons/hotel_management_system/hotel_book_order.py
@@ -8,7 +8,7 @@ class hotel_book_order(osv.Model):
         'first_name': fields.char('First Name', size=128, required=True, ),
         'last_name': fields.char('Last Name', size=128, required=True,),
         'gender': fields.selection([('m', 'Male'), ('f', 'Female')], 'Gender', ),
-        'guest_type': fields.many2one('hotel.guest.type', 'Guest Type',),
+        'guest_type': fields.many2one('hotel.guest.type', 'Guest Type',required=True),
         'country_id': fields.many2one('res.country', 'Country',),
         'room_id': fields.many2one('hotel.room', 'Room Number', required=True, ondelete='cascade',),
         # 'room_id_view':fields.function(_get_auto_room_id_view, string='Room Number', type='char',store=True),
