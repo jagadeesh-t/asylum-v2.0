6003e25520c286768387e5d61e59e0ab5891d996
thresholdsoftware <deepu.rao90@gmail.com>
2015-05-10 12:02:27 +0530

    Transfer Guest

--- openerp/addons/hotel_management_system/wizard/transfer_guest.py
+++ openerp/addons/hotel_management_system/wizard/transfer_guest.py
@@ -6,3 +6,9 @@ class transfer_guest(osv.TransientModel):
     _columns = {
         'room_id': fields.many2one('hotel.room', 'Room Number', required=True),
     }
+
+    def transfer_room(self, cr, uid, ids, context=None):
+        for active_ids in context['active_ids']:
+            for transfer_guest in self.browse(cr, uid, ids):
+                self.pool.get("hotel.book.order").write(cr, uid, [active_ids], {'room_id':transfer_guest.room_id.id},context=None)
+        return True
