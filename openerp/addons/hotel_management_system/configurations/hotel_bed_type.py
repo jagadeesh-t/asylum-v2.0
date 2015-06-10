8c4db4c9de0169a84f937bf45f8c8a890f9fe74f
khudrath <alibaigkhudrath@gmail.com>
2015-06-11 01:04:20 +0530

    capacity must be non zero

--- openerp/addons/hotel_management_system/configurations/hotel_bed_type.py
+++ openerp/addons/hotel_management_system/configurations/hotel_bed_type.py
@@ -38,10 +38,18 @@ class hotel_bed_type(osv.osv):
         else:
             return False
 
+    def _constraints_capacity_nonzero(self, cr, uid, ids, context=None):
+        result =True
+        for current_rec in self.browse(cr, uid, ids):
+            if current_rec.value <= 0:
+                result = False
+        return result
+
     _columns = {
         'name': fields.char('Type', size=128, required=True, select=True),
-        'value': fields.integer
-        ('Capacity', help="1 for Single, 2 for Double. This will be used for counting available Spaces in a room"),
+        'value': fields.integer('Capacity',
+                                help="1 for Single, 2 for Double. " +
+                                     "This will be used for counting available Spaces in a room"),
         'total_stock': fields.integer('Total Stock', help="Total Stock"),
         'available_stock': fields.function(_available_stock, type='integer', string='Available Stock', store=False),
         'used_stock': fields.function(_used_stock, type='integer', string='Used Stock', store=False),
@@ -51,6 +59,7 @@ class hotel_bed_type(osv.osv):
     ]
     _constraints = [
         (_check_avi_total_stock, 'Error! Requested Number of Bed Are Not Decreaseble.', ['total_stock']),
+        (_constraints_capacity_nonzero, 'Capacity must be greater than zero.', ['Capacity']),
     ]
 
 hotel_bed_type()
