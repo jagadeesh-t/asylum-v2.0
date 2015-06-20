8acd933a25fd6e8a47cfd1a2ab2e8bc84ccc33e6
thresholdsoftware <deepu.rao90@gmail.com>
2015-06-21 00:26:43 +0530

    Access Rights

--- openerp/addons/hotel_management_system/__openerp__.py
+++ openerp/addons/hotel_management_system/__openerp__.py
@@ -17,7 +17,8 @@
     'depends': ['base', 'board', 'web_printscreen_zb'],
     'data': [
         'hotel_menu.xml',
-
+        'security/hotel_security.xml',
+        'security/ir.model.access.csv',
         'wizard/transfer_guest_view.xml',
         'wizard/add_bed_qty_view.xml',
         'wizard/reduce_bed_qty_view.xml',
