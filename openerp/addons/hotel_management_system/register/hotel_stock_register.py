831337050ee19aab19bee2ad05a998c0ea3b3445
thresholdsoftware <deepu.rao90@gmail.com>
2015-07-30 17:02:38 +0530

    Stock Static Date Base Validation

--- openerp/addons/hotel_management_system/register/hotel_stock_register.py
+++ openerp/addons/hotel_management_system/register/hotel_stock_register.py
@@ -9,19 +9,22 @@ class hotel_stock_register(osv.Model):
     """
     def onchange_stock_statistics(self, cr, uid, ids, to_date,from_date, context=None ):
         res = {}
+        total_points=0.0
         if to_date != False and from_date != False:
             line=[]
-            cr.execute('select hpl.product_id,sum(hpl.qty) as qty,sum(hpl.qty*hpl.pts_unit) as pts from hotel_purchase hp inner join hotel_purchase_lines hpl on hp.id = inv_id where hp.state  = \'done\' group by hpl.product_id',())
+            cr.execute('select hpl.product_id,sum(hpl.qty) as qty,sum(hpl.qty*hpl.pts_unit) as pts from hotel_purchase hp inner join hotel_purchase_lines hpl on hp.id = inv_id where hp.state  = \'done\'  and date(date) >= %s and date(date)<= %s group by hpl.product_id',(to_date, from_date,))
             for t in cr.dictfetchall():
                 line.append([0, False, {'product_category': t['product_id'], 'pts': t['pts'], 'qty': t['qty']}])
+                total_points=total_points+float(t['pts'])
             res['inv_lines']= line
+            res['total_points']= total_points
             return {'value': res}
-        else:
-            warning = {
-                    'title': _('Warning!'),
-                    'message': _('The selected unit of measure is not compatible with the unit of measure of the product.')
-                }
-            return {'warning': warning}
+        # else:
+        #     warning = {
+        #             'title': _('Warning!'),
+        #             'message': _('The selected unit of measure is not compatible with the unit of measure of the product.')
+        #         }
+        #     return {'warning': warning}
 
     _name = "hotel.stock.register"
     _description = "Stock Registeer"
@@ -29,6 +32,7 @@ class hotel_stock_register(osv.Model):
     _columns = {
         'to_date': fields.date('To Date', required=True,),
         'from_date': fields.date('From Date', required=True,),
+        'total_points':fields.float('Total Points',digits=(14, 3)),
         'inv_lines': fields.one2many('hotel.stock.statistics.register', 'inv_id', 'Purchase Order Lines',),
         }
 
