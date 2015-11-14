abb086512d277616623966f8f714813c8dd7ae1a
thresholdsoftware <deepu.rao90@gmail.com>
2015-11-14 20:26:34 +0530

    Combo box is added for guest selection

--- openerp/addons/hotel_management_system/hotel_book_order.py
+++ openerp/addons/hotel_management_system/hotel_book_order.py
@@ -32,9 +32,7 @@ class hotel_book_order(osv.Model):
     _name = "hotel.book.order"
     _description = "Hotel Book Order"
     _columns = {
-        'guest_ref': fields.char('Guest Ref.', size=128, required=True,),
-        'first_name': fields.char('First Name', size=128, required=True, ),
-        'last_name': fields.char('Last Name', size=128, required=True,),
+        'guest_id': fields.many2one('hotel.guest.partner', 'Guest Name', required=True),
         'gender': fields.selection([('m', 'Male'), ('f', 'Female')], 'Gender', ),
         'guest_type': fields.many2one('hotel.guest.type', 'Guest Type',required=True),
         'country_id': fields.many2one('res.country', 'Country',),
