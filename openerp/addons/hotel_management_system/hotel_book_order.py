e3ff76dd243a2ace84bff4d95acf9aec20cc9cdb
thresholdsoftware <deepu.rao90@gmail.com>
2016-05-01 19:18:48 +0530

    Search Implementation

--- openerp/addons/hotel_management_system/hotel_book_order.py
+++ openerp/addons/hotel_management_system/hotel_book_order.py
@@ -30,9 +30,26 @@ class hotel_book_order(osv.Model):
             res[order.id] = name
         return res
 
+    def _get_auto_name(self, cr, uid, ids, field_name, arg, context=None):
+        """
+        @return: Dictionary of values.
+        """
+        all_orders=self.search(cr,uid,[],context=context)
+        orders = self.browse(cr, uid, all_orders, context=context)
+        res = {}
+        for order in orders:
+            name =""
+            name = order.guest_id.name + " " + order.guest_id.last_name +" ["+ order.guest_id.guest_ref +"]"
+            res[order.id] = name
+        return res
+
     _name = "hotel.book.order"
     _description = "Hotel Book Order"
+    _order = "name"
     _columns = {
+        'name':fields.function(_get_auto_name, string='Name', type='char',store=True),
+        'first_name': fields.char('First Name', size=128, required=True, select=True, readonly=True, states={'cin': [('readonly', False)]}),
+        'last_name': fields.char('Last Name', size=128, required=True, select=True, readonly=True, states={'cin': [('readonly', False)]}),
         'guest_id': fields.many2one('hotel.guest.partner', 'Guest Name', required=True),
         'guest_type': fields.many2one('hotel.guest.type', 'Guest Type',required=True),
         'country_id': fields.many2one('res.country', 'Country',),
